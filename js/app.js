fb1bddba729ed4b304af0c4e16e6480999606a4b
Kate Simmons <kate.a.simmons@gmail.com>
2015-08-20 16:53:39 -0500

    files created, connected

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+var question = {
+  questionNumber: "1" + ".  ",
+  questionText: "What color is the sky? <br><br>",
+  answers: ["Blue", "Green", "Yellow"],
+  buildQuestion: function() {
+    $('.question').append(this.questionNumber + this.questionText + this.answers);
+  }
+}
+
+question.buildQuestion();
+
+});
