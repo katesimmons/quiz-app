90d330186031533a985dbaba523cd84cc0b7cc6a
Kate Simmons <kate.a.simmons@gmail.com>
2015-08-28 08:48:14 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-/*--
-
-var question = {
-  questionNumber: "1" + ".  ",
-  questionText: "What color is the sky? <br><br>",
-  answers: ["Blue", "Green", "Yellow"],
-  buildQuestion: function() {
-    $('.question').append(this.questionNumber + this.questionText + this.answers);
-  }
-}
-
-question.buildQuestion();
---*/
-
 $(document).ready(function() {
 
 /*----VARIABLES----*/
